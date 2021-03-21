90c95d902de8c789f43fcf944ac51e06c1113a74
rajc90 <rchphekar@live.com>
2021-03-20 21:58:45 -0700

    Added GDP Data

--- appServer.R
+++ appServer.R
@@ -1,1 +1,12 @@
+library(dplyr)
+
+GDPdata <- read.csv("GDP.csv") %>%
+  rename(Year = "ï..Year")
+
+data <- read.csv("Minimum Wage Data Cleaned.csv") %>%
+  mutate(DistanceFromFedWage = Federal.Minimum.Wage.2020.Dollars - State.Minimum.Wage.2020.Dollars)
+
+data <- left_join(data, GDPdata)
+colnames(data)
+
 server<-function(input, output){}
--- appUI.R
+++ appUI.R
@@ -1,1 +1,13 @@
 ui <- tabPanel()
+## Have a page that explores the nominal wages of years compared to the CPI
+    # Nominal Wages vs CPI
+    # Nominal Wages vs 2020 $
+    # 2020 wage vs 2020 Fed
+    # Projections of Future Wages based on current data
+
+## Have page analyzing the effect of the wage on real gdp
+  # plot real GDP and Wages
+  # try to find correlation
+  # create an equation
+
+## have a map visualizationof minimum wage (above/below federal)
