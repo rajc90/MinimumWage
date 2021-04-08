2ca78b4ab9887ed44e346cb26da60b66cddfa240
rajc90 <rchphekar@live.com>
2021-04-08 15:34:50 -0700

    Added background info

--- introTab.R
+++ introTab.R
@@ -3,6 +3,23 @@ intro <- tabPanel(
     "Intro"
     ),
   h2(id = "BodyHeaders",
+     "Background"
+  ),
+  div( id = "IntroText",
+       p("The federal minimum wage has been a hotly debated topuic in the current 
+         political climate. Set in 1939, the federal minimum wage has risen from 25 cents
+         to $7.25 as of April 2021. Many Americans believe that the minimum wage should be 
+         higher than $7.25, increasing to $15. Their arguments for this increase are the 
+         increase in economic activity from the increase in income. They also think increasing
+         minimum wage will reduce poverty and reduce government spending. However, there are 
+         many opponents of the increase believe that businesses would have to lay off more 
+         employee with increased wages raising unemployment. This would in turn increase
+         poverty and hurt businesses, increasing governemtn spending on welfare.
+         This dashboard will analyze the change of minimum wage at a federal and state level, along with
+         factors like the CPI and GDP.")
+  ),
+  
+  h2(id = "BodyHeaders",
      "The Raw Data"
     ),
   div( id = "IntroText",
--- minWageMap.R
+++ minWageMap.R
@@ -3,3 +3,14 @@ minWageMapTab <- tabPanel(
       "Minimum Wage Map"
   ),
 )
+GDPdata <- read.csv("GDP.csv") %>%
+  rename(Year = "ï..Year")
+
+data <- read.csv("Minimum Wage Data Cleaned.csv") %>%
+  mutate(DistanceFromFedWage = Federal.Minimum.Wage.2020.Dollars - State.Minimum.Wage.2020.Dollars)
+data <- left_join(data, GDPdata)
+
+Yearly_Values <- read.csv("Changes in Federal Values.csv")
+
+
+ggplot(data)
